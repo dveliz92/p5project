06dd81bd73020d4ab2f0754f90ccdf899afcee1f
dveliz92 <dveliz92@gmail.com>
2021-05-03 04:29:41 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var motion=5
+//The setup function only happens once
+function setup() {
+	createCanvas(500, 500); //create a 500px X 500px canvas
+}
+
+function draw() {
+  background(0); 
+  //this a flashlight
+    fill(255, 255, 0, 100);
+   ellipse(mouseX,mouseY,50,50);
+    fill(255, 255, 0, 60);
+  ellipse(mouseX,mouseY,100,100);
+    fill(255, 255, 0, 100);
+  ellipse(mouseX,mouseY,150,150);
+    //body 
+    fill(mouseX,mouseY);
+  rect(motion,100,150,175);
+  circle(motion+85,100,100);
+
+}
