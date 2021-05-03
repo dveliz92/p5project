1bd371a85ac268e4ef58e95d573d38d5b535bfb3
dveliz92 <dveliz92@gmail.com>
2021-05-03 19:33:22 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,26 @@
 var motion=5
-//The setup function only happens once
 function setup() {
-	createCanvas(500, 500); //create a 500px X 500px canvas
+	createCanvas(500, 500);
 }
 
+
 function draw() {
-  background(0); 
-  //this a flashlight
-    fill(255, 255, 0, 100);
+  background(0);
+//character moving and blending 
+  fill(mouseX,mouseY)
+    if (motion>300){
+    motion=0
+  }else{
+    motion=motion+1;
+  }
+  //flashlight
+  rect(motion+150,200,40,100)
+  ellipse(motion+175,200,50,50)
+ fill(255, 255, 0, 100);
    ellipse(mouseX,mouseY,50,50);
     fill(255, 255, 0, 60);
   ellipse(mouseX,mouseY,100,100);
     fill(255, 255, 0, 100);
   ellipse(mouseX,mouseY,150,150);
-    //body 
-    fill(mouseX,mouseY);
-  rect(motion,100,150,175);
-  circle(motion+85,100,100);
-  if (motion>300){
-    motion=0
-  }else{
-    motion=motion+1;
-  }
-  
+
 }
